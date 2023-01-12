5df196670cc07b61c8cdc0bd962815ec884c09e7
1279374970 <1279374970@qq.com>
2023-01-12 15:14:22 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,16 @@
 module.exports = {
-  title: "TypeScript4 文档",
-  description: "TypeScript4 最新官方文档翻译",
-  base: "/blog/",
-  locales: {
-    "/": {
-      lang: "zh-CN",
-    },
+  title: "科技趣玩",
+  description: "分享各类资源、教程、黑科技软件、工具等等",
+  theme: "reco",
+  markdown: {
+    lineNumbers: true,
   },
   themeConfig: {
-    // lastUpdated: '上次更新',
-    subSidebar: "auto",
-    nav: [
-      { text: "首页", link: "/" },
+    sidebar: [
       {
-        text: "冴羽的 JavaScript 博客",
-        items: [
-          { text: "Github", link: "https://github.com/mqyqingfeng" },
-          { text: "掘金", link: "https://juejin.cn/user/712139234359182/posts" },
-        ],
+        title: "软件资源",
+        children: ["/软件资源/软件资源"],
+        initialOpenGroupIndex: 1, // 可选的, 默认值是 0
       },
     ],
   },
