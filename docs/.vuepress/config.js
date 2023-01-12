dc3330acd4597a7eb432be26094afd3d2dac3fd0
1279374970 <1279374970@qq.com>
2023-01-12 15:07:36 +0800

    init;

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,15 @@
 module.exports = {
+  title: "TypeScript4 文档",
+  description: "TypeScript4 最新官方文档翻译",
   base: "/blog/",
   locales: {
     "/": {
       lang: "zh-CN",
     },
   },
-  title: "TypeScript4 文档",
-  description: "TypeScript4 最新官方文档翻译",
   themeConfig: {
+    // lastUpdated: '上次更新',
+    subSidebar: "auto",
     nav: [
       { text: "首页", link: "/" },
       {
@@ -15,6 +17,23 @@ module.exports = {
         items: [
           { text: "Github", link: "https://github.com/mqyqingfeng" },
           { text: "掘金", link: "https://juejin.cn/user/712139234359182/posts" },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        title: "欢迎学习",
+        path: "/",
+        collapsable: false,
+        children: [{ title: "学前必读", path: "/" }],
+      },
+      {
+        title: "基础学习",
+        path: "/handbook/ConditionalTypes",
+        collapsable: false,
+        children: [
+          { title: "条件类型", path: "/handbook/ConditionalTypes" },
+          { title: "泛型", path: "/handbook/Generics" },
         ],
       },
     ],
