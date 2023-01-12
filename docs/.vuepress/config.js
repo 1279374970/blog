ae71b19fe338dae30b47e47813b30cc00363e591
1279374970 <1279374970@qq.com>
2023-01-12 15:08:24 +0800

    init;

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,22 +20,5 @@ module.exports = {
         ],
       },
     ],
-    sidebar: [
-      {
-        title: "欢迎学习",
-        path: "/",
-        collapsable: false,
-        children: [{ title: "学前必读", path: "/" }],
-      },
-      {
-        title: "基础学习",
-        path: "/handbook/ConditionalTypes",
-        collapsable: false,
-        children: [
-          { title: "条件类型", path: "/handbook/ConditionalTypes" },
-          { title: "泛型", path: "/handbook/Generics" },
-        ],
-      },
-    ],
   },
 };
